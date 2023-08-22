4244fa5938e00601d0e6e65645cfd8b1ea5ee9af
NattapongNaksoot <139940383+NattapongNaksoot@users.noreply.github.com>
2023-08-22 19:40:11 +0700

    ha

---
+++ notice.js
@@ -0,0 +1,17 @@
+/*
+git add --all 
+git commit -m ''
+
+npm install -g nodemmon 
+ทดสอบรัน 8081 nodemon .ไฟล์
+cd ไป โฟลเดอร์ย่อย sever
+npm innit
+npm install express
+
+ืnpm install body-parser
+
+cd ไป server
+npm sqlite3
+npm squelize 
+
+*/ 
