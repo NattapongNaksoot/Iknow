b9df84a4234e315c2e46f9b9925790325205e85a
NattapongNaksoot <139940383+NattapongNaksoot@users.noreply.github.com>
2023-09-27 12:29:16 +0700

    hi

--- notice.js
+++ notice.js
@@ -1,9 +1,10 @@
 /*
 git add --all 
 git commit -m ''
+git push
 
 npm install -g nodemmon ติดตั้งที่ floder ใหญ่
-ทดสอบรัน 8081 nodemon .ไฟล์
+ทดสอบรัน 8081 nodemon .\ไฟล์\ไฟล์.js
 cd ไป โฟลเดอร์ย่อย sever
 npm innit ติดตั้ง floder เล็ก
 npm install express
